598657c871be5d86803e4d8af3f69e01b974e63e
SeleneLozano <lozanoselen@gmail.com>
2022-10-07 08:57:36 -0500

    Cambios finales de la tarea 1, al final no se pudo resolver el error  net.SGD( training_data, 30, 10, 4.0, 0.3, test_data=test_data) TypeError: SGD() got multiple values for argument 'test_data'

--- network.py
+++ network.py
@@ -1,17 +1,5 @@
 # %load network.py
 
-"""
-network.py
-~~~~~~~~~~
-IT WORKS
-
-A module to implement the stochastic gradient descent learning
-algorithm for a feedforward neural network.  Gradients are calculated
-using backpropagation.  Note that I have focused on making the code
-simple, easily readable, and easily modifiable.  It is not optimized,
-and omits many desirable features.
-"""
-
 #### Libraries
 # Standard library
 import random
@@ -19,24 +7,32 @@ import random
 # Third-party libraries
 import numpy as np
 
+"""Agregando Cross Entropy"""
+class CrossEntropyCost(object):
+    
+    @staticmethod
+    def fn(a, y):
+      
+        return np.sum(np.nan_to_num(-y*np.log(a)-(1-y)*np.log(1-a)))
+
+    @staticmethod
+    def delta(z, a, y):
+     
+        return (a-y)
+
+
 class Network(object):
 
     def __init__(self, sizes):
-        """The list ``sizes`` contains the number of neurons in the
-        respective layers of the network.  For example, if the list
-        was [2, 3, 1] then it would be a three-layer network, with the
-        first layer containing 2 neurons, the second layer 3 neurons,
-        and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+       
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+        self.velocidad = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
+
 
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
@@ -46,14 +42,6 @@ class Network(object):
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             test_data=None):
-        """Train the neural network using mini-batch stochastic
-        gradient descent.  The ``training_data`` is a list of tuples
-        ``(x, y)`` representing the training inputs and the desired
-        outputs.  The other non-optional parameters are
-        self-explanatory.  If ``test_data`` is provided then the
-        network will be evaluated against the test data after each
-        epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially."""
 
         training_data = list(training_data)
         n = len(training_data)
@@ -74,21 +62,26 @@ class Network(object):
             else:
                 print("Epoch {} complete".format(j))
 
-    def update_mini_batch(self, mini_batch, eta):
-        """Update the network's weights and biases by applying
-        gradient descent using backpropagation to a single mini batch.
-        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-        is the learning rate."""
+    def update_mini_batch(self, mini_batch, eta, fric):
+        """ fric; fricción, ya que se incluye en la definición de momento """
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w-(eta/len(mini_batch))*nw
-                        for w, nw in zip(self.weights, nabla_w)]
+    
+        self.velocidad = [fric*v-(eta/len(mini_batch))*nw
+                        for v, nw in zip(self.velocidad, nabla_w)]
+        self.weights = [w+v
+                        for w, v in zip(self.weights,self.velocidad)]
+        """
+        Se usa 
+        SGD con momento, usando la definición
+        """
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
+
 
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
